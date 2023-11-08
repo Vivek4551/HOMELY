be3e96a1b95e230d711a2fda652bbcac809988c8
Vivek4551 <workwithvivek70@gmail.com>
2023-11-09 00:36:45 +0530

    second day

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
-/*
-`   how to create nested elements ?
-    <div id="parent">
-        <div id="child">
-            <h1>I'm H1 tag</h1>
-        </div>
-    </div>
-*/
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
-// const parent = React.createElement(
-//     "div", 
-//     {id:"parent"},
-//     React.createElement(
-//         "div",
-//         {id:"child"},
-//         React.createElement("h1",{},"I'm H1 tag")
-//     )
-// );
-
-/*
-`   how to create nested elements along with siblngs?
-    <div id="parent">
-        <div id="child">
-            <h1>I'm H1 tag</h1>
-            <h2>I'm H@ tag</h2>
-        </div>
-    </div>
-*/
 // if we want to pass more than one children then we have to pass it like an array
 const parent = React.createElement(
     "div", 
@@ -33,7 +8,7 @@ const parent = React.createElement(
     React.createElement(
         "div",
         {id:"child"},[
-        React.createElement("h1",{},"I'm H1 tag"), 
+        React.createElement("h1",{},"This is Namaste React 🚀"), 
         React.createElement("h2",{},"I am H2 tag")
     ])
 );
@@ -48,5 +23,5 @@ const parent = React.createElement(
 console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-        
+
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,12 @@
+## Namaste React 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement (Follows File watching algorithm)
+- File Watching Algorithm - written in c++ 
+- Caching - Faster Builds (.Parcel-cache)
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
