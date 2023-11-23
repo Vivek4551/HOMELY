5cefbcb485bd223ddc359e4e1e474e0463f3adc9
Vivek4551 <workwithvivek70@gmail.com>
2023-11-23 23:03:29 +0530

    crossproxy updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,4 +8,6 @@ export const LOGO_URL =
 
 // two types of export -> named export and normal export
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.2548238&lng=75.7001618&restaurantId=";
+export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.2548238&lng=75.7001618&restaurantId=";
+
+// if we want to use extension the crossproxy.io is not needed as the link has restrictions over 40 API calls in 1 minute so we use the extension to bypass that restriction.
